3f5400e38a67077f734de49b2e2408feae0c8b11
David Gardiner <david@gardiner.net.au>
2015-02-21 14:09:39 +1030

    Comments
    SQL Injection example
    
    More queries

--- BadPractise.Wpf/MainPageViewModel.cs
+++ BadPractise.Wpf/MainPageViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
@@ -17,12 +19,65 @@ namespace BadPractise.Wpf
             Products = new ObservableCollection<ProductDto>();
             Inventory = new ObservableCollection<string>();
 
+            // Opening lots of connections
+            for (var i = 0; i < 10; i++)
+            {
+                using (var context = new MyDbContext())
+                {
+                    context.Products.FirstOrDefault();
+                }
+            }
+
+
             using (var context = new MyDbContext())
             {
                 // SELECT * FROM
                 foreach (var product in context.Products)
                 {
-                    Products.Add(new ProductDto {Id = product.ProductId, Name = product.Name});
+                    Products.Add(new ProductDto
+                    {
+                        Id = product.ProductId,
+                        Name = product.Name
+                    });
+                }
+
+                // TODO - Move these into separate query classes and use output in app
+
+                // Eager loading
+                var products = context.Products
+                    .Include(p => p.ProductSubcategory)
+                    .Include(p => p.ProductSubcategory.ProductCategory)
+                    .Select(p => p).ToList();
+
+                // parameters are different sizes
+                var p1 = context.Database.SqlQuery<Product>("SELECT * FROM Production.Product WHERE Name = @P0", "Five")
+                    .ToList();
+
+                var p2 =
+                    context.Database.SqlQuery<Product>("SELECT * FROM Production.Product WHERE Name = @P0",
+                        "TwentyEight")
+                        .ToList();
+
+                // SQL injection
+                    context.Database.SqlQuery<Product>(string.Format("SELECT * FROM Production.Product WHERE Name = '{0}'", "UserData"))
+                        .ToList();
+
+                // query begins with %
+                context.Products.Where(p => p.Name.Contains("b")).ToList();
+
+                // read entire table
+                context.Products.ToList();
+
+                // N+1
+                var salesPeople = from sp in context.SalesPersons
+                    select sp;
+
+                foreach (var salesPerson in salesPeople)
+                {
+                    foreach (var store in salesPerson.Stores)
+                    {
+                        Debug.WriteLine(store.Name);
+                    }
                 }
             }
         }
@@ -35,7 +90,8 @@ namespace BadPractise.Wpf
             get { return _selectedProduct; }
             set
             {
-                if (Equals(value, _selectedProduct)) return;
+                if (Equals(value, _selectedProduct))
+                    return;
                 _selectedProduct = value;
 
                 LoadInventoryForProduct(_selectedProduct.Id);
@@ -68,7 +124,8 @@ namespace BadPractise.Wpf
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             var handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
